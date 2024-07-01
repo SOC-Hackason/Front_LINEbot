e480fad6462f82c11206e926162f4acaa02901d9
Amano-take <63344408+Amano-take@users.noreply.github.com>
2024-07-01 20:17:54 +0900

    test: try to remove ""

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def webhook():
             f"https://mails.amano.mydns.jp/gmail/emails/summary?line_id={user_id}"
         )
         response_text = res.text
-        print(res.text)
+        print(response_text)
         messages = [
             {
                 "type": "text",
