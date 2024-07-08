474af63d2f28201cf1d150371cb40e155ebc8b3f
Amano Remote <takehiro0927@icloud.com>
2024-07-08 21:29:10 +0900

    add: 「一覧」

--- main.py
+++ main.py
@@ -26,15 +26,40 @@ def webhook():
 
     event = events[0]
     reply_token = event.get("replyToken")
-    received_message = event["message"].get("text")
     user_id = event["source"].get("userId")
-    
-    print(user_id)
-    sys.stdout.flush()
-
+    try:
+        received_message = event["message"].get("text")
+        messages = message_reply(user_id, received_message)
+    except:
+        postback_data = event["postback"].get("data")
+        loading_spinner(user_id)
+        messages = postback_reply(user_id, postback_data)
     if not reply_token:
         return jsonify({"status": "no reply token"}), 200
 
+    
+        
+
+    headers = {
+        "Content-Type": "application/json; charset=UTF-8",
+        "Authorization": f"Bearer {CHANNEL_TOKEN}",
+    }
+
+    payload = {
+        "replyToken": reply_token,
+        "messages": messages,
+    }
+
+    response = requests.post(REPLY_URL, headers=headers, json=payload)
+
+    if response.status_code != 200:
+        print(response.text)
+        sys.stdout.flush()
+        return jsonify({"status": "error", "detail": response.text}), 500
+
+    return jsonify({"status": "success"}), 200
+
+def message_reply(user_id, received_message):
     if received_message == "認証":
         messages = [
             {
@@ -45,6 +70,9 @@ def webhook():
     elif received_message == "要約":
         loading_spinner(user_id)
         messages = summary_reply(user_id)
+    elif received_message == "一覧":
+        loading_spinner(user_id)
+        messages = list_message(user_id)
     elif received_message == "既読":
         res = requests.get(
             f"https://mails.amano.mydns.jp/gmail/emails/read?line_id={user_id}"
@@ -66,25 +94,135 @@ def webhook():
     else:
         # get 
         messages = free_message(received_message, user_id)
-        
 
-    headers = {
-        "Content-Type": "application/json; charset=UTF-8",
-        "Authorization": f"Bearer {CHANNEL_TOKEN}",
+    return messages
+
+def postback_reply(user_id, postback_data):
+    if postback_data.startswith("msg_id"):
+        msg_id = postback_data.split("=")[1]
+        url = BACKEND_URL + "/gmail/emails"
+        params = {
+            "msg_id": msg_id,
+            "line_id": user_id
+        }
+        response = requests.get(url, params=params)
+        data = response.json()
+        messages = flex_one_mail(data)
+    return messages
+
+def flex_one_mail(data):
+    _from = data["from"]
+    _to = data["to"]
+    _subject = data["subject"]
+    _message = data["message"]
+
+    bubble = {
+        "type": "bubble",
+        "body": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+                {
+                    "type": "text",
+                    "text": _from,
+                    "weight": "bold",
+                    "size": "sm"
+                },
+                {
+                    "type": "text",
+                    "text": _to,
+                    "weight": "bold",
+                    "size": "sm"
+                },
+                {
+                    "type": "text",
+                    "text": _subject,
+                    "weight": "bold",
+                    "size": "sm"
+                },
+                {
+                    "type": "text",
+                    "text": _message,
+                    "wrap": True
+                }
+            ]
+        }
     }
 
-    payload = {
-        "replyToken": reply_token,
-        "messages": messages,
+    messages = [
+        {
+            "type": "flex",
+            "altText": "メール詳細",
+            "contents": bubble
+        }
+    ]
+
+    return messages
+
+def list_message(line_id):
+    url = BACKEND_URL + "/gmail/unread_titles"
+    params = {
+        "line_id": line_id,
+        "max_results": 12
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    titles = data["message"]
+    msg_ids = data["msg_ids"]
+
+    flex_contents = []
+    for title, msg_id in zip(titles, msg_ids):
+        flex_contents.append({
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+                {
+                    "type": "text",
+                    "text": title,
+                    "wrap": True,
+                    "weight": "bold",
+                    "size": "xs",
+                    "maxLines": 2,
+                    "flex": 3,
+                },
+                {
+                    "type": "button",
+                    "action": {
+                        "type": "postback",
+                        "label": "詳細",
+                        "data": f"msg_id={msg_id}"
+                    },
+                    "style": "primary",
+                    "color": "#905c44",
+                    "height": "sm",
+                    "margin": "sm",
+                    "flex": 1,
+                }
+            ],
+            "alignItems": "center",
+            "paddingAll": "sm",
+            "margin": "xs",
+        })
+
+    bubble = {
+        "type": "bubble",
+        "body": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": flex_contents
+        }
     }
 
-    response = requests.post(REPLY_URL, headers=headers, json=payload)
+    messages = [
+        {
+            "type": "flex",
+            "altText": "メール一覧",
+            "contents": bubble
+        }
+    ]
 
-    if response.status_code != 200:
-        return jsonify({"status": "error", "detail": response.text}), 500
-
-    return jsonify({"status": "success"}), 200
-
+    return messages
+           
 def free_message(sentence, line_id):
     url = f"https://mails.amano.mydns.jp/gmail/free_sentence?line_id={line_id}"
     data = {
@@ -150,7 +288,7 @@ def loading_spinner(user_id):
 
     data = {
         "chatId": user_id,
-        "loadingSeconds": 5
+        "loadingSeconds": 10
     }
 
     response = requests.post(url, headers=headers, json=data)
