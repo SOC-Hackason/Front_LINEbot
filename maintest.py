61f4bb4b72e38311b0d1790a1b67aea012a6c97c
Tanned-podcast <kftwitter2020@gmail.com>
2024-07-09 10:30:36 +0900

    ブロックするemailリストをユーザーから受け取ってリストに格納できるようにした

--- maintest.py
+++ maintest.py
@@ -1,8 +1,9 @@
 from app.env import *
+import json, os
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, FlexSendMessage, TextSendMessage
+from linebot.models import MessageEvent, PostbackEvent, TextMessage, FlexSendMessage, TextSendMessage
 
 app = Flask(__name__)
 
@@ -12,6 +13,8 @@ LINE_CHANNEL_SECRET = CHANNEL_SECRET
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+blocklist=[]
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -29,153 +32,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if event.message.text == "Flex Message":
-        flex_message = {
-            "type": "bubble",
-            "hero": {
-                "type": "image",
-                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-                "size": "full",
-                "aspectRatio": "20:13",
-                "aspectMode": "cover",
-                "action": {
-                    "type": "uri",
-                    "uri": "http://linecorp.com/"
-                }
-            },
-            "body": {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
-                    {
-                        "type": "text",
-                        "text": "Brown Cafe",
-                        "weight": "bold",
-                        "size": "xl"
-                    },
-                    {
-                        "type": "box",
-                        "layout": "baseline",
-                        "margin": "md",
-                        "contents": [
-                            {
-                                "type": "icon",
-                                "size": "sm",
-                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                            },
-                            {
-                                "type": "icon",
-                                "size": "sm",
-                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                            },
-                            {
-                                "type": "icon",
-                                "size": "sm",
-                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                            },
-                            {
-                                "type": "icon",
-                                "size": "sm",
-                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                            },
-                            {
-                                "type": "icon",
-                                "size": "sm",
-                                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-                            },
-                            {
-                                "type": "text",
-                                "text": "4.0",
-                                "size": "sm",
-                                "color": "#999999",
-                                "margin": "md",
-                                "flex": 0
-                            }
-                        ]
-                    },
-                    {
-                        "type": "box",
-                        "layout": "vertical",
-                        "margin": "lg",
-                        "spacing": "sm",
-                        "contents": [
-                            {
-                                "type": "box",
-                                "layout": "baseline",
-                                "spacing": "sm",
-                                "contents": [
-                                    {
-                                        "type": "text",
-                                        "text": "Place",
-                                        "color": "#aaaaaa",
-                                        "size": "sm",
-                                        "flex": 1
-                                    },
-                                    {
-                                        "type": "text",
-                                        "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
-                                        "wrap": True,
-                                        "color": "#666666",
-                                        "size": "sm",
-                                        "flex": 5
-                                    }
-                                ]
-                            },
-                            {
-                                "type": "box",
-                                "layout": "baseline",
-                                "spacing": "sm",
-                                "contents": [
-                                    {
-                                        "type": "text",
-                                        "text": "Time",
-                                        "color": "#aaaaaa",
-                                        "size": "sm",
-                                        "flex": 1
-                                    },
-                                    {
-                                        "type": "text",
-                                        "text": "10:00 - 23:00",
-                                        "wrap": True,
-                                        "color": "#666666",
-                                        "size": "sm",
-                                        "flex": 5
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                ]
-            },
-            "footer": {
-                "type": "box",
-                "layout": "vertical",
-                "spacing": "sm",
-                "contents": [
-                    {
-                        "type": "button",
-                        "style": "link",
-                        "height": "sm",
-                        "action": {
-                            "type": "uri",
-                            "label": "CALL",
-                            "uri": "https://linecorp.com"
-                        }
-                    },
-                    {
-                        "type": "button",
-                        "style": "link",
-                        "height": "sm",
-                        "action": {
-                            "type": "uri",
-                            "label": "WEBSITE",
-                            "uri": "https://linecorp.com"
-                        }
-                    }
-                ],
-                "flex": 0
-            }
-        }
+    if event.message.text == "f":
+        #jsonファイルではtrueは小文字
+        with open(os.path.join(os.path.dirname(__file__), "Addresslist.json"), "r") as j:
+            flex_message = json.load(j)
 
         flex_message = FlexSendMessage(
             alt_text='Flex Message',
@@ -186,11 +46,22 @@ def handle_message(event):
             event.reply_token,
             flex_message
         )
+    elif event.message.text == "以上":
+        line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=str(blocklist))
+        )
     else:
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="「Flex Message」と入力してください。")
         )
+
+@handler.add(PostbackEvent)
+def handle_postback(event):
+    user_id = event.source.user_id
+    data = event.postback.data
+    blocklist.append(data)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8000)
