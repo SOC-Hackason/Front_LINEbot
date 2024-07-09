9db45d877b116d6ef02f485394d83667f49f427d
Tanned-podcast <kftwitter2020@gmail.com>
2024-07-09 17:50:58 +0900

    deleted sensitive files

--- app/env.py
+++
@@ -1,4 +0,0 @@
-CHANNEL_TOKEN = "+upv04InwMYYzFrlS58h1dJcoHdX7C8V7+fsboZ/NVSwzVFDNkPb1SSH5m7bMkvgQyZzoaF5Wx8oMrgHbbhJtweAaKseioakTgOjYCN0k/+0Xv0cjZWIrTRlzsJdJ8xMLqEKko1HVwokOlmLNTVGuQdB04t89/1O/w1cDnyilFU="
-CHANNEL_SECRET = "62ff9249bec39b8a14e2d6ec24a3f721"
-REPLY_URL = 'https://api.line.me/v2/bot/message/reply'
-ADD_BP = True
--- app/env_KF.py
+++
@@ -1,5 +0,0 @@
-#個人CH(KFch)で開発するときはここを参照すること
-CHANNEL_TOKEN = "TPGkmsMDfqrZDn76opfIcewAahpZm2KhGFDX03oONkEM+5JtBdKTNA0itYT28RlVTWAouh+EMsAcGalOqnxzNFUPwWB2tsoLgQwFxvNG0tQl4LF4Fx9HvrS9mqhc2ob3GoQjhwK25JKasHAiuj20JAdB04t89/1O/w1cDnyilFU="
-CHANNEL_SECRET = "7eb22bcce23b4075e374dccb6740402b"
-REPLY_URL = 'https://api.line.me/v2/bot/message/reply'
-ADD_BP = True
